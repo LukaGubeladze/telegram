e9d9ca0bb4b1f832214348239dde9a509065adc0
LukaGubeladze <l.gubeladze@a1qa.com>
2024-12-12 18:44:27 +0400

    1

---
+++ Telegram/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Task 1: Number greater than 7
+        Console.Write("Enter a number: ");
+        if (int.TryParse(Console.ReadLine(), out int number) && number > 7)
+        {
+            Console.WriteLine("Hello");
+        }
+
+        // Task 2: Name check
+        Console.Write("Enter a name: ");
+        string name = Console.ReadLine();
+        if (name == "John")
+        {
+            Console.WriteLine("Hello, John");
+        }
+        else
+        {
+            Console.WriteLine("There is no such name");
+        }
+
+        // Task 3: Numeric array
+        Console.Write("Enter numbers separated by spaces: ");
+        int[] numbers = Console.ReadLine()?.Split(' ').Select(int.Parse).ToArray() ?? Array.Empty<int>();
+        Console.WriteLine("Multiples of 3:");
+        foreach (int n in numbers.Where(n => n % 3 == 0))
+        {
+            Console.WriteLine(n);
+        }
+
+        // Bonus: Bracket sequence question
+        Console.WriteLine("\nBracket sequence analysis:");
+        string sequence = "[((())()(())]]";
+        Console.WriteLine($"Original sequence: {sequence}");
+        Console.WriteLine("Correct sequence: [((())()(())])");
+    }
+}
